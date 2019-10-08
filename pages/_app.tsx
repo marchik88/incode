01a293d05a42b5b1545eb15a567f687f39ccf1e9
Security2431 <Artem2431@gmail.com>
2019-10-08 18:03:30 +0200

    chore: configure apollo

---
+++ pages/_app.tsx
@@ -0,0 +1,38 @@
+import App from "next/app";
+import Head from "next/head";
+import React from "react";
+import { ApolloProvider } from "react-apollo";
+import withApollo from "../lib/withApollo";
+
+type AppProps = {
+  apolloClient: any;
+};
+
+class MyApp extends App<AppProps> {
+  static async getInitialProps({ Component, ctx }: any) {
+    let pageProps = {};
+
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+
+    return { pageProps };
+  }
+
+  render() {
+    const { Component, pageProps, apolloClient } = this.props;
+
+    return (
+      <>
+        <Head>
+          <title>Incode test</title>
+        </Head>
+        <ApolloProvider client={apolloClient}>
+          <Component {...pageProps} />
+        </ApolloProvider>
+      </>
+    );
+  }
+}
+
+export default withApollo(MyApp);
